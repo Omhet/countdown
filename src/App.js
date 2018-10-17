680c9854aa3999d61eee91fabc8737feaebdc6c7
Omhet <omhetehmo@yandex.ru>
2018-10-17 09:31:52 +0400

    create cardwrapper

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// My Components
+import CardWrapper from './components/basic/CardWrapper';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+          <CardWrapper />
       </div>
     );
   }
---
+++ src/components/basic/CardWrapper.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import styles from './CardWrapper.css';
+
+class CardWrapper extends Component {
+    render() {
+        return (
+            <div className={styles}>
+                CardWrapper
+            </div>
+        );
+    }
+}
+
+export default CardWrapper;
