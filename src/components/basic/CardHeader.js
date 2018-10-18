289bfda58e7396793eab82cf63be582e9d72ff4d
Omhet <omhetehmo@yandex.ru>
2018-10-18 21:41:36 +0400

    WordCard with basic input

--- src/components/basic/CardHeader.js
+++ src/components/basic/CardHeader.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from "react-redux";
+
 import {Header} from "semantic-ui-react";
+import * as cardNames from '../../constants/cardNames'
+
+const mapStateToProps = state => {
+  return { name: state.currentCard.name };
+};
 
 class CardHeader extends Component {
+    getDescription = () => {
+        switch (this.props.name) {
+            case cardNames.WORD_CARD:
+                return 'Найди слово'
+            case cardNames.NUMBER_CARD:
+                return 'Найди число'
+            default:
+                return '';
+        }
+    }
     render() {
+        const description = this.getDescription();
+
         return (
             <div>
-                <Header size='large'>Найди число</Header>
+                <Header size='large'>{description}</Header>
             </div>
         );
     }
 }
 
-export default CardHeader;
+export default connect(mapStateToProps, null)(CardHeader);
--- src/components/basic/CardWrapper.js
+++ src/components/basic/CardWrapper.js
@@ -1,22 +1,56 @@
 import React, { Component } from 'react';
+import { connect } from "react-redux";
+
 import style from './CardWrapper.css';
+import baseStyle from './Base.css';
 
 import Rate from './Rate';
 import CardHeader from './CardHeader';
 import Timer from './Timer';
 import CancelButton from './CancelButton';
 import DoneButton from './DoneButton';
+import WordCard from '../cards/WordCard';
+import NumberCard from '../cards/NumberCard';
+import * as cardNames from '../../constants/cardNames';
+
+import * as actionCreators from '../../actions/index';
+
+const mapStateToProps = state => {
+  return { level: state.level };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        setCardName: name => dispatch(actionCreators.setCardName(name))
+    }
+};
+
+const mapCardNamesToComponents = {
+    WordCard: <WordCard/>,
+    NumberCard: <NumberCard/>,
+};
 
 class CardWrapper extends Component {
+    resolveCard = () => {
+        const cardName = (this.props.level % 2 === 0) ? cardNames.WORD_CARD : cardNames.NUMBER_CARD;
+        this.props.setCardName(cardName);
+        return mapCardNamesToComponents[cardName];
+    }
+
     render() {
+        const currentCard = this.resolveCard();
+
         return (
             <div className={style.cardwrapper}>
-                <div className={style.upper}>
+                <div className={baseStyle.borderBottom}>
                     <Rate />
                     <CardHeader />
                     <Timer />
                 </div>
-                <div className={style.bottom}>
+
+                { currentCard }
+
+                <div>
                     <CancelButton />
                     <DoneButton />
                 </div>
@@ -25,4 +59,4 @@ class CardWrapper extends Component {
     }
 }
 
-export default CardWrapper;
+export default connect(mapStateToProps, mapDispatchToProps)(CardWrapper);
