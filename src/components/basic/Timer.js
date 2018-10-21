67f2dfe21a4580dc68ce048bb5975ab4da69d3f5
Omhet <omhetehmo@yandex.ru>
2018-10-21 17:04:16 +0400

    timer ticks, changes value on level started

--- src/components/basic/Timer.js
+++ src/components/basic/Timer.js
@@ -1,22 +1,44 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {Header, Icon} from "semantic-ui-react";
 
 import BaseStyle from './Base.css';
 import {connect} from "react-redux";
 
 const mapStateToProps = state => {
-    return { maxTime: state.maxTime }
+    return {maxTime: state.maxTime, levelStarted: state.level.started}
 };
 
 class Timer extends Component {
     state = {
+        timer: null,
         currentTime: this.props.maxTime
     };
+
+    componentDidMount() {
+        let timer = setInterval(this.tick, 1000);
+        this.setState({timer});
+    }
+
+    componentWillUnmount() {
+        this.clearInterval(this.state.timer);
+    }
+
+    tick = () => {
+        if (!this.props.levelStarted) {
+            this.setState({
+                currentTime: this.props.maxTime
+            });
+        } else {
+            this.setState({
+                currentTime: this.state.currentTime - 1
+            });
+        }
+    }
 
     render() {
         return (
             <div className={BaseStyle.flex}>
-                <Icon name='time' />
+                <Icon name='time'/>
                 <Header className={BaseStyle.nomargin} size='medium'>{this.state.currentTime}</Header>
             </div>
         );
--- src/reducers/time.js
+++ src/reducers/time.js
@@ -1,6 +1,6 @@
 import {SET_MAX_TIME} from "../constants/actionTypes";
 
-export default function maxTime(state = -1, action) {
+export default function maxTime(state = 30, action) {
     switch (action.type) {
         case SET_MAX_TIME:
             return action.time;
